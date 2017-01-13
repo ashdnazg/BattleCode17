21f13b92606532f87e5080d25f3b8ab2c331e44a
Eshed <ashdnazg@gmail.com>
2017-01-13 22:51:10 +0200

    remove copy pasta

--- src/johnny4/Counter.java
+++ src/johnny4/Counter.java
@@ -4,16 +4,6 @@ import battlecode.common.*;
 
 public class Counter {
 
-    //Integer 0:        Archon Counter (0-7) Unit Counter (8-15) Enemy Counter (16-23)
-    //Integer 1-3:      Archon Info
-    //Integer 4-100:    Unit Info
-    //Integer 101-200:  Enemy Info
-    //Integer 201-300:  Enemy Trees
-    //Integer 301-320:  Requested trees to remove according to priority
-    //Integer 321:      ALARM ALARM
-
-    //Info: X (0-9) Y (10-19) Type (20-22) Timestamp (23-31)
-
     static RobotController rc;
 
     public static int increment(int index) throws GameActionException {
